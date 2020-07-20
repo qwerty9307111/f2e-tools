64a0229b5d524b6e7a660c17e9c90a532334ebfe
yww <2436415068@qq.com>
2020-07-20 14:24:35 +0800

    feat: 密码正则优化

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 import tools from './lib/index.js'
 
-export default tools
+let exportTool = tools
+
+const proxyFn = (target, key, receiver, targetKey) => {
+  if (key.indexOf(targetKey) >= 0 && key !== targetKey) {
+    const lastStr = key.split(targetKey + '_')[1] || ''
+    const [startIndex, endIndex] = lastStr.split('_')
+    return target[targetKey][startIndex][endIndex]
+  }
+  return target[key]
+}
+
+if (Proxy) {
+  exportTool = new Proxy(tools, {
+    get (target, key, receiver) {
+      if (key.indexOf('REGEXP_PASS_COMPLEX_') >= 0) {
+        return proxyFn(target, key, receiver, 'REGEXP_PASS_COMPLEX')
+      }
+      if (key.indexOf('REGEXP_PASS_MODERATE_') >= 0) {
+        return proxyFn(target, key, receiver, 'REGEXP_PASS_MODERATE')
+      }
+      return target[key]
+    }
+  })
+}
+
+export default exportTool
